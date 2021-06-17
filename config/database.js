6b0542bc60c33db2b3f9951366d194de11ecacb2
bemakonate <professorkonate@gmail.com>
2021-06-17 11:14:19 -0400

    Setup application for production

--- config/database.js
+++ config/database.js
@@ -1,15 +1,37 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'development') {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
+      }
+    }
+
+  } else {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'mongoose',
+          settings: {
+            client: 'mongo',
+            uri: `mongodb+srv://${env('DB_USER')}:${env('DB_PASSWORD')}@cluster0.xrbrr.mongodb.net/${env('DB_NAME')}?retryWrites=true&w=majority`
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+    }
+
+  }
+}
